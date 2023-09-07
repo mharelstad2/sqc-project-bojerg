ecd9fb3150d8a4ad107d422883245fe570d1b3e3
bojerg <bojerg@users.noreply.github.com>
2023-09-06 23:21:47 -0500

    Express server and public/index.html

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+const app = express()
+const port = process.env.PORT || 5163
+
+app.use(express.static('./public/'))
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`)
+})
+
+//adapted from https://expressjs.com/en/starter/hello-world.html
