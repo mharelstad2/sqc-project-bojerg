471e5487d79dcee0c4a2e15b8f0858fcf4cd955e
bojerg <>
2023-09-07 21:32:46 -0500

    Basic style and dark mode

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,17 @@
-$( document ).ready(function() {
-	//code here
-});
+$(document).ready(function() {
+	$(".bars-icon").on("click", function() {
+		$(".nav-bars-dropdown").toggle()
+	})
+	
+	$("#dark-mode-btn").on("click", function() {
+		if($("body").hasClass("dark")) {
+			$("body").removeClass("dark")
+			$("header").removeClass("dark-accent")
+			$(".nav-bars-dropdown").removeClass("dark-accent")
+		} else {
+			$("body").addClass("dark")
+			$("header").addClass("dark-accent")
+			$(".nav-bars-dropdown").addClass("dark-accent")
+		}
+	})
+})
