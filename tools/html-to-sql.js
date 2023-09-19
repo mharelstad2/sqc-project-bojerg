437c88aed3756f35e4ee629fdbddf815149086ea
bojerg <bojerg@users.noreply.github.com>
2023-09-18 23:54:40 -0500

    Linted html to sql tool and finished parsing prototype

--- tools/html-to-sql.js
+++ tools/html-to-sql.js
@@ -1,6 +1,5 @@
 import { parse } from 'node-html-parser'
-import { closeSync, openSync, readFileSync, writeFileSync }
-  from 'node:fs'
+import { closeSync, openSync, readFileSync, writeFileSync } from 'node:fs'
 
 const dstPath = 'docs/generated-schema.sql'
 const src = readFileSync('data/book.html', 'utf8')
@@ -45,12 +44,12 @@ INSERT INTO element_type (description)
 
 INSERT INTO chapter (title, number) VALUES
 `
-const sqlInsertSection = `INSERT INTO section (chapter_id, title, number) VALUES`
-const sqlInsertElement = `INSERT INTO element (section_id, element_type_id, index, content) VALUES`
+const sqlInsertSection = 'INSERT INTO section (chapter_id, title, number) VALUES'
+const sqlInsertElement = 'INSERT INTO element (section_id, element_type_id, index, content) VALUES'
 
 // Breaking down the DOM of book.html
-let bookParts = domRoot.querySelectorAll('.chapter')
-let chapters = {}
+const bookParts = domRoot.querySelectorAll('.chapter')
+const chapters = {}
 
 // Only want content from the Guide portion of the book
 const startFlag = 'I. Guide'
@@ -60,67 +59,73 @@ let collectDivs = false
 bookParts.forEach(
   (div) => {
     let title = ''
-    let headers = div.querySelectorAll("h2, h3")
-    if(headers.length > 0) {
+    const headers = div.querySelectorAll('h2, h3')
+    if (headers.length > 0) {
       title = headers[0].innerText
     }
-    if(title == stopFlag) {
+
+    // collectDivs set to true at bottom of function, so we don't collect the "I. Guide" header node
+    if (title === stopFlag) {
       collectDivs = false
-    } else if(collectDivs) { // collectDivs set to true at bottom of function
-	  //Start collecting HTML and additional required data
+    } else if (collectDivs) { // HERE Start collecting HTML and additional required data
       // Fix img paths
       div.querySelectorAll('img').forEach(
-      (image) => {
-        const oldSrc = image.getAttribute('src')
-        const oldSrcTokens = oldSrc.split('/')
-        const newSrc = `/images/book/${oldSrcTokens[oldSrcTokens.length - 1]}`
-        image.setAttribute('src', newSrc)
+        (image) => {
+          const oldSrc = image.getAttribute('src')
+          const oldSrcTokens = oldSrc.split('/')
+          const newSrc = `/images/book/${oldSrcTokens[oldSrcTokens.length - 1]}`
+          image.setAttribute('src', newSrc)
+        }
+      )
+
+      // Clean up collected HTML
+      div.innerHTML = div.innerHTML.replaceAll('\r\n', '\n')
+      div.innerHTML = div.innerHTML.trim()
+
+      // Each h2/h3 tag has an anchor tab named something like chap01 or chap01.4 ...
+      // We split into two parts, chapter and section
+      const chapterAndSection = headers[0].querySelector('a').id.split('.')
+
+      // remove the chap from the number
+      chapterAndSection[0] = Number(chapterAndSection[0].substring(4))
+
+      if (chapterAndSection.length === 1) {
+        chapterAndSection.push(0) // If no section listed, we are working with chapter intro (section 0 for our purposes)
+      } else {
+        chapterAndSection[1] = Number(chapterAndSection[1])
       }
-    )
 
-    div.innerHTML = div.innerHTML.replaceAll('\r\n', '\n')
-    div.innerHTML = div.innerHTML.trim()
+      // Add chapter to object if not exists
+      if (!(chapterAndSection[0] in chapters)) {
+        chapters[chapterAndSection[0]] = {}
+        chapters[chapterAndSection[0]].title = title
+        chapters[chapterAndSection[0]].sections = {}
+      }
 
-    // Each h2/h3 tag has an anchor tab named something like chap01 or chap01.4 ...
-	// We split into two parts, chapter and section
-	const chapterAndSection = headers[0].querySelector('a').id.split('.')
-	
-	// remove the chap from the number 
-	chapterAndSection[0] = Number(chapterAndSection[0].substring(4))
-	
-	if(chapterAndSection.length == 1) {
-		chapterAndSection.push(0) //If no section listed, we are working with chapter intro
-	} else {
-		chapterAndSection[1] = Number(chapterAndSection[1])
-	}
-	
-	// Add chapter to object if not exists
-	if(!chapters.hasOwnProperty(chapterAndSection[0])) {
-		chapters[chapterAndSection[0]] = {}
-		chapters[chapterAndSection[0]].title = title
-		chapters[chapterAndSection[0]].sections = {}
-	}
-	
-	// Add section to object (reminder, treating intro to chapter as section 0)
-	chapters[chapterAndSection[0]].sections[chapterAndSection[1]] = {}
-	chapters[chapterAndSection[0]].sections[chapterAndSection[1]].title = title
-	chapters[chapterAndSection[0]].sections[chapterAndSection[1]].parts = {}
-	
-	let elementCount = 1
-	div.childNodes.forEach(
-	  (e) => {
-		// Will need to determine element_type at a later date
-		
-		
-		
-		elementCount++
-	  }
-	)
-	
+      // Add section to object (reminder, treating intro to chapter as section 0)
+      chapters[chapterAndSection[0]].sections[chapterAndSection[1]] = {}
+      chapters[chapterAndSection[0]].sections[chapterAndSection[1]].title = title
+      chapters[chapterAndSection[0]].sections[chapterAndSection[1]].parts = {}
 
+      let elementCount = -1
+      div.childNodes.forEach(
+        (e) => {
+        // Will need to determine element_type at a later date
+          if (elementCount !== -1) { // Skip the first as title has been extracted already
+            chapters[chapterAndSection[0]].sections[chapterAndSection[1]].parts.index = elementCount
+
+            // TODO detect type and store appropriate data into content
+            chapters[chapterAndSection[0]].sections[chapterAndSection[1]].parts.content = e.innerText
+            // Calling everything a paragraph for prototyping's sake
+            chapters[chapterAndSection[0]].sections[chapterAndSection[1]].parts.type_id = 0
+          }
+          elementCount++
+        }
+      )
     }
 
-    if(title == startFlag) {
+    // Stop collecting at the start of the reference section
+    if (title === startFlag) {
       collectDivs = true
     }
   }
