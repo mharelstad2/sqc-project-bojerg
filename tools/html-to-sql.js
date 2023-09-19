0a25d67509c86a15c745ce9f6a1099ddf98c2065
bojerg <bojerg@users.noreply.github.com>
2023-09-19 01:51:49 -0500

    Finished converting JS obj to SQL statements and writing to SQL file

--- tools/html-to-sql.js
+++ tools/html-to-sql.js
@@ -5,15 +5,14 @@ const dstPath = 'docs/generated-schema.sql'
 const src = readFileSync('data/book.html', 'utf8')
 const domRoot = parse(src)
 const sqlHeader = `SET client_encoding = 'UTF8';
-DROP TABLE IF EXISTS chapter;
-DROP TABLE IF EXISTS section;
-DROP TABLE IF EXISTS element_type;
+DROP TABLE IF EXISTS chapter CASCADE;
+DROP TABLE IF EXISTS section CASCADE;
+DROP TABLE IF EXISTS element_type CASCADE;
 DROP TABLE IF EXISTS element;
 
 CREATE TABLE chapter (
   id SERIAL PRIMARY KEY,
-  title TEXT NOT NULL,
-  number INT NOT NULL
+  title TEXT NOT NULL
 );
 
 CREATE TABLE section (
@@ -31,7 +30,7 @@ CREATE TABLE element_type (
 
 CREATE TABLE element (
   id SERIAL PRIMARY KEY,
-  section_id TEXT NOT NULL,
+  section_id INT NOT NULL,
   element_type_id INT NOT NULL,
   index INT NOT NULL,
   content TEXT NOT NULL,
@@ -40,9 +39,16 @@ CREATE TABLE element (
 );
 
 INSERT INTO element_type (description)
-  VALUES (paragraph, letter, footnote, noindent, table, code, non_section_header, image);
+  VALUES ('paragraph'),
+         ('letter'),
+         ('footnote'),
+         ('no_indent'),
+         ('diagram'),
+         ('code'), 
+         ('non_section_header'),
+         ('image');
 
-INSERT INTO chapter (title, number) VALUES
+INSERT INTO chapter (title) VALUES
 `
 const sqlInsertSection = 'INSERT INTO section (chapter_id, title, number) VALUES'
 const sqlInsertElement = 'INSERT INTO element (section_id, element_type_id, index, content) VALUES'
@@ -107,28 +113,111 @@ bookParts.forEach(
       chapters[chapterAndSection[0]].sections[chapterAndSection[1]].title = title
       chapters[chapterAndSection[0]].sections[chapterAndSection[1]].parts = {}
 
-      let elementCount = -1
+      let index = -1
       div.childNodes.forEach(
-        (e) => {
+        (part) => {
         // Will need to determine element_type at a later date
-          if (elementCount !== -1) { // Skip the first as title has been extracted already
-            chapters[chapterAndSection[0]].sections[chapterAndSection[1]].parts.index = elementCount
+          if (index !== -1) { // Skip the first as title has been extracted already
+            chapters[chapterAndSection[0]].sections[chapterAndSection[1]].parts[index] = {}
 
             // TODO detect type and store appropriate data into content
-            chapters[chapterAndSection[0]].sections[chapterAndSection[1]].parts.content = e.innerText
+            chapters[chapterAndSection[0]].sections[chapterAndSection[1]].parts[index].content = part.innerText
             // Calling everything a paragraph for prototyping's sake
-            chapters[chapterAndSection[0]].sections[chapterAndSection[1]].parts.type_id = 0
+            chapters[chapterAndSection[0]].sections[chapterAndSection[1]].parts[index].type_id = 1
           }
-          elementCount++
+          index++
         }
       )
     }
 
-    // Stop collecting at the start of the reference section
+    // Start collecting at the beginning of the guide section
     if (title === startFlag) {
       collectDivs = true
     }
   }
 )
 
-console.log(chapters)
+
+
+// Now, we need to insert our JavaScript object into a completed SQL statement
+// The insert statement for chapters was already started in sqlHeader
+const chapNums = Object.keys(chapters)
+const fd = openSync(dstPath, 'w')
+writeFileSync(fd, sqlHeader)
+
+// Insert chapters
+writeFileSync(fd, `('${chapters[chapNums[0]].title}')`)
+chapNums.slice(1).forEach(
+  (key) => {
+    writeFileSync(fd, `,\n('${chapters[key].title}')`)
+  }
+)
+writeFileSync(fd, ';\n\n')
+
+// Insert sections -- (chapter_id, title, number)
+let first = true
+writeFileSync(fd, sqlInsertSection)
+chapNums.forEach(
+  (chapKey) => {
+    const chapter = chapters[chapKey]
+    const sectNums = Object.keys(chapter.sections)
+    if(first) {
+      writeFileSync(fd, `('${chapKey}', '${chapter.sections[0].title}', '${sectNums[0]}')`)
+      sectNums.slice(1).forEach(
+        (sectKey) => {
+          writeFileSync(fd, `,\n('${chapKey}', '${chapter.sections[sectKey].title}', '${sectKey}')`)
+        }
+      )
+
+      first = false
+
+    } else {
+      sectNums.forEach(
+        (sectKey) => {
+          writeFileSync(fd, `,\n('${chapKey}', '${chapter.sections[sectKey].title}', '${sectKey}')`)
+        }
+      )
+    }
+  }
+)
+writeFileSync(fd, ';\n\n')
+
+// Insert elements (AKA "parts") -- (section_id, element_type_id, index, content)
+// Note: I regret naming a database table the same name as a restricted word in JavaScript!
+let sectId = 1
+first = true // Piggy-backing sentinel variable from previous loop
+writeFileSync(fd, sqlInsertElement)
+chapNums.forEach(
+  (chapKey) => {
+    const chapter = chapters[chapKey]
+    const sectNums = Object.keys(chapter.sections)
+    sectNums.forEach(
+      (sectKey) => {
+        const section = chapter.sections[sectKey]
+        const partIndicies = Object.keys(section.parts)
+        if(first) {
+          writeFileSync(fd, `('${sectId}', '${section.parts[0].type_id}', '${partIndicies[0]}', '${section.parts[0].content}')`)
+          partIndicies.slice(1).forEach(
+            (i) => {
+              writeFileSync(fd, `,\n('${sectId}', '${section.parts[i].type_id}', '${i}', '${section.parts[i].content}')`)
+            }
+          )
+
+          first = false
+
+        } else {
+          partIndicies.forEach(
+            (i) => {
+              writeFileSync(fd, `,\n('${sectId}', '${section.parts[i].type_id}', '${i}', '${section.parts[i].content}')`)
+            }
+          )
+        }
+
+        sectId++
+      }
+    )
+  }
+)
+writeFileSync(fd, ';\n\n')
+
+closeSync(fd)
