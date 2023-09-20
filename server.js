fc851ab77cff40584e0d1cc861ebbc49109a393f
bojerg <bojerg@users.noreply.github.com>
2023-09-20 00:31:36 -0500

    Setting up view engine EJS

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const port = process.env.PORT || 5163
 
 app.use(express.static('./public/'))
+app.set('views', 'views')
+app.set('view engine', 'ejs')
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
